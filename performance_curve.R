331df9adf8f993385b5065116a138370dcae33c7
Rohan McLure <rohanmclure@gmail.com>
2016-06-27 11:49:58 +1000

    Used user time in preference to system time.

--- performance_curve.R
+++ performance_curve.R
@@ -3,10 +3,12 @@
 
 # tests is the size of the data being input.
 # samplesize is the number of trials allocated to each load size.
+
+
 benchmark <- function(testsizes, nsamples)
 {
 	samplemeans <- c()
-	
+
 	for (i in 1:length(testsizes))
 	{
 		dataset = c()
@@ -14,11 +16,11 @@ benchmark <- function(testsizes, nsamples)
 		for (trial in 1:nsamples)
 		{
 			# Populates the entry for an input size with its trial time.
-			dataset[trial] <- divisibility_bench(testsizes[i])
+			dataset[trial] <- do.call(divisibility_bench,(testsizes[i])
 		}
 
 		print(dataset)
-	
+
 		samplemeans[i] <- mean(dataset)
 	}
 
@@ -34,8 +36,8 @@ matrix_arithmetic_bench <- function(size)
 	b <- rand_matrix(FALSE, size)
 
 	# Solves matrix, plots contents of variable vector X.
-	dt <- system.time(X <- solve(A,b))[3]
-	dt <- dt + system.time(barplot(1:length(X),X))[3]
+	dt <- system.time(X <- solve(A,b))[1]
+	dt <- dt + system.time(barplot(1:length(X),X))[1]
 	return (dt)
 }
 
@@ -45,30 +47,31 @@ matrix_multiplication_bench <- function(size)
 	A <- rand_matrix(TRUE, size)
 	B <- rand_matrix(TRUE, size)
 
-	return (system.time(A %*% B)[3])
+	return (system.time(A %*% B)[1])
 }
 
 rand_matrix <- function(is_matrix, slength)
 {	
 	n = slength
-	
+
 	if (is_matrix)
 	{
 		n <- n ** 2
 	}
-	
+
 	# Generates a line of members for the matrix.
 	members <- sample(-10:10, size=n, replace=TRUE)
 
 	return( matrix((members), nrow=slength ))
 }
 
+# Reasonable test data: 5000000*30:50
 divisibility_bench <- function(size)
 {
 	data <- sample(-100:100, size, replace=TRUE)
 	moduli <- sample(1:10, size, replace=TRUE)
 
-	return (system.time(data %% moduli)[3])
+	return (system.time(data %% moduli)[1])
 }
 
 incrementation_bench <- function(size)
@@ -76,7 +79,7 @@ incrementation_bench <- function(size)
 	# Vector of 0's of amount size.
 	data <- rep(0,size)
 
-	return (system.time(data <- data + 1))
+	return (system.time(data <- data + 1)[1])
 }
 
 vector_column_synthesis_bench <- function(size)
@@ -88,8 +91,8 @@ vector_column_synthesis_bench <- function(size)
 		vectors[i] <- rand_matrix(FALSE, size)
 	}
 
-	return (system.time(cbind(vectors)))
+	return (system.time(cbind(vectors))[1])
 }
 
 
-benchmark(500000*10:30, 2)
+benchmark(5000000*30:50, 1)
